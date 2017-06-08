ff24b75f5298b5d50b694ad6e4d4b9ee36e1f342
Edward Kemper <edwardkemper@gmail.com>
2017-06-08 18:08:27 -0500

    first commit

---
+++ server/main.js
@@ -0,0 +1,10 @@
+// import { Meteor } from 'meteor/meteor';
+
+// // import '../imports/ui/body.js';
+
+// Meteor.startup(() => {
+//   // code to run on server at startup
+// });
+
+
+import '../imports/api/tasks.js';
